79d9ad7e0ad41400de0af12266fc6517d7410705
vilgefortz <vilgefortz@mainfrm.com>
2015-02-08 10:51:04 +0100

    some js code removal

--- WebContent/scripts/sidebar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$.post ("Article",{action:"getArticles"}, function (data) {
-		data=$.parseJSON(data);
-		$.each(data, function (key, value) {
-			value=$.parseJSON(value);
-			$("#articles").append("<li><a href='index.jsp?m=article&a=display&id=" + value.id + "'>" + value.date +  " " + value.title + "</a></li>");
-		});
-	});
-});
